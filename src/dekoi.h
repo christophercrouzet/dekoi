47453b32a35d2cbecad4546e304b1799e6d42cd7
Christopher Crouzet <christopher.crouzet@gmail.com>
2017-11-01 16:14:25 +0700

    Add window manager macros
    This allows an user to query what window manager is going to be used.

--- src/dekoi.h
+++ src/dekoi.h
@@ -42,6 +42,23 @@
  DK_STATIC_ASSERT(0, platform_not_supported);
 #endif
 
+#if defined(DK_PLATFORM_ANDROID)
+ #define DK_WINDOW_MANAGER_ANDROID
+#elif defined(DK_PLATFORM_IOS) || defined(DK_PLATFORM_MACOS)
+ #define DK_WINDOW_MANAGER_COCOA
+#elif defined(DK_PLATFORM_LINUX)
+ #if defined(DK_USE_WAYLAND)
+  #define DK_WINDOW_MANAGER_WAYLAND
+ #else
+  #define DK_WINDOW_MANAGER_X11
+ #endif
+#elif defined(DK_PLATFORM_WINDOWS)
+ #define DK_WINDOW_MANAGER_WINDOWS
+#else
+ DK_STATIC_ASSERT(0, window_manager_not_supported);
+ #define DK_WINDOW_MANAGER_INVALID
+#endif
+
 /*
    Focus on the common ILP32, LP64 and LLP64 data models.
    64-bit integer types aren't part of C89 but should be available anyways.
